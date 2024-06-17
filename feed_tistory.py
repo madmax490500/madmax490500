551ecaf0d4d012486ecdadcd470cf0a6c59b29a6
madmax00 <124117831+madmax490500@users.noreply.github.com>
2024-06-17 16:38:21 +0900

    Update feed_tistory.py

--- feed_tistory.py
+++ feed_tistory.py
@@ -1,16 +1,12 @@
-import feedparser
-import time
+import feedparser, time
 
-def parse_feed(url):
-    rss_feed = feedparser.parse(url)
-    if rss_feed.bozo:
-        raise Exception(f"Error parsing feed: {rss_feed.bozo_exception}")
-    return rss_feed
+URL = "https://bitnaleeeee.github.io/feed.xml"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST = 5
 
-def generate_markdown(entries, max_post):
-    markdown_text = """
+markdown_text = """
 ### 시스템엔지니어가 사용하는 코드 저장소입니다.
-### This repository is used by SRE.
+### This repository use by SRE.
 
 ### 지속적인 목표 (SRE 목표)
 * 서비스의 안정성을 유지하면서 변화를 최대한 수용한다.
@@ -22,43 +18,15 @@ def generate_markdown(entries, max_post):
 
 
 ## ✅ Latest Blog Posts
-
 """  # list of blog posts will be appended here
 
-    for idx, feed in enumerate(entries):
-        if idx >= max_post:
-            break
-        feed_date = feed.get('published_parsed')
-        if feed_date:
-            formatted_date = time.strftime('%Y/%m/%d', feed_date)
-        else:
-            formatted_date = "Unknown date"
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
         
-        title = feed.get('title', 'No title')
-        link = feed.get('link', '#')
-        
-        markdown_text += f"[{formatted_date} - {title}]({link}) <br/>\n"
-    
-    return markdown_text
-
-def write_to_file(content, file_path):
-    with open(file_path, mode="w", encoding="utf-8") as f:
-        f.write(content)
-        print(f"{file_path} file has been written.")
-
-def main():
-    url = "https://vitta.tistory.com/rss"
-    max_post = 5
-
-    rss_feed = parse_feed(url)
-    entries = rss_feed.get('entries', [])
-    print(f"Number of entries: {len(entries)}")
-
-    markdown_text = generate_markdown(entries, max_post)
-    print("Generated markdown text:")
-    print(markdown_text)
-
-    write_to_file(markdown_text, "README.md")
-
-if __name__ == "__main__":
-    main()
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
